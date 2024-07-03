86964c877ee736dff82952e1ec8f7e7ae2b780ff
mac <mac@Shivam-MacBook-Pro.local>
2024-07-03 19:59:54 +0530

    fix the cursor animation

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Sticky Navigation Menu JS Code
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
-console.log(scrollBtn);
-let val;
+
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 20) {
     nav.classList.add("sticky");
@@ -13,11 +12,12 @@ window.onscroll = function () {
   }
 };
 
-// Side NavIgation Menu JS Code
+// Side Navigation Menu JS Code
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
+
 menuBtn.onclick = function () {
   navBar.classList.add("active");
   menuBtn.style.opacity = "0";
@@ -25,6 +25,7 @@ menuBtn.onclick = function () {
   body.style.overflow = "hidden";
   scrollBtn.style.pointerEvents = "none";
 };
+
 cancelBtn.onclick = function () {
   navBar.classList.remove("active");
   menuBtn.style.opacity = "1";
@@ -35,7 +36,8 @@ cancelBtn.onclick = function () {
 
 // Side Navigation Bar Close While We Click On Navigation Links
 let navLinks = document.querySelectorAll(".menu li a");
-for (var i = 0; i < navLinks.length; i++) {
+
+for (let i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click", function () {
     navBar.classList.remove("active");
     menuBtn.style.opacity = "1";
@@ -43,41 +45,54 @@ for (var i = 0; i < navLinks.length; i++) {
   });
 }
 
-// type js plugin
+// Type JS Plugin
 let typeJsText = document.querySelector(".typeJsText");
 let lines = typeJsText.dataset.typetext.split("|");
 let lineIndex = 0;
-let charIndex = -1;
+let charIndex = 0;
 
 typeJsText.innerHTML = "";
 
-function typeJs() {
-  if (charIndex < lines[lineIndex].length - 1) {
-    charIndex++;
-    typeJsText.innerHTML += lines[lineIndex].charAt(charIndex);
-  } else if (lineIndex < lines.length - 1) {
-    charIndex = -1;
-    lineIndex++;
-    typeJsText.innerHTML += "<br>";
+function type() {
+  if (lineIndex < lines.length) {
+    if (charIndex < lines[lineIndex].length) {
+      // Join lines up to the current line being typed
+      let textToShow = lines.slice(0, lineIndex).join("<br>");
+      if (lineIndex > 0) textToShow += "<br>";
+      textToShow +=
+        lines[lineIndex].substring(0, charIndex + 1) +
+        '<span class="cursor">|</span>';
+
+      typeJsText.innerHTML = textToShow;
+      charIndex++;
+      setTimeout(type, 150); // Adjust typing speed here (in milliseconds)
+    } else {
+      charIndex = 0;
+      lineIndex++;
+      setTimeout(type, 1000); // Delay before typing the next line (in milliseconds)
+    }
   } else {
-    typeJsText.innerHTML = "";
     lineIndex = 0;
-    charIndex = -1;
+    charIndex = 0;
+    setTimeout(type, 1000); // Restart typing after a delay (in milliseconds)
   }
 }
 
-setInterval(() => {
-  typeJs();
-}, 200);
+type();
 
+// Function to open Gmail with a pre-filled recipient
 function openGmail() {
   const email = "shivamagrawal1june@gmail.com";
-
   const url = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}`;
 
-  window.open(url, "_blank");
+  if (navigator.userAgent.match(/iPhone|iPad|iPod|Android/i)) {
+    window.location.href = url;
+  } else {
+    window.open(url, "_blank");
+  }
 }
 
+// Function to download CV
 function downloadCV() {
   const link = document.createElement("a");
   link.href = "Resume.pdf"; // Path to your resume file
